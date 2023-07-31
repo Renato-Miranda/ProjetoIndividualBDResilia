da953299422137a4408c8e9ad8f45e4b9bc84572
Renato <renatomna@gmail.com>
2023-07-30 21:05:09 -0300

    feat: adição de comentários

--- biblioteca_tabelas.sql
+++ biblioteca_tabelas.sql
@@ -1,5 +1,9 @@
+------------------ Criação de do banco de dados
+
 CREATE DATABASE biblioteca_renato;
 USE biblioteca_renato;
+
+-- ----------------Criação da entidade de pessoas
 
 CREATE TABLE `pessoas` (
   `id_pessoa` integer PRIMARY KEY,
@@ -10,6 +14,8 @@ CREATE TABLE `pessoas` (
   `data_nascimento` date,
   `endereco_id` integer
 );
+
+-- ------------------Criação da entidade de endereços
 
 CREATE TABLE `enderecos` (
   `id_endereco` integer PRIMARY KEY,
@@ -23,6 +29,8 @@ CREATE TABLE `enderecos` (
   `pais` varchar(20)
 );
 
+-- ------------------Criação da entidade de funcionários
+
 CREATE TABLE `funcionarios` (
   `id_funcionario` integer PRIMARY KEY,
   `cargo` varchar(25),
@@ -31,17 +39,23 @@ CREATE TABLE `funcionarios` (
   `pessoa_id` integer
 );
 
+-- -----------------Criação da entidade de usuarios
+
 CREATE TABLE `usuarios` (
   `id_usuario` integer PRIMARY KEY,
   `senha` varchar(20),
   `pessoa_id` integer
 );
 
+-- -----------------Criação da entidade de autores
+
 CREATE TABLE `autores` (
   `id_autor` integer PRIMARY KEY,
   `qtd_livros` integer,
   `pessoa_id` integer
 );
+
+-- ----------------Criação da entidade de livros
 
 CREATE TABLE `livros` (
   `id_livros` integer PRIMARY KEY,
@@ -52,11 +66,15 @@ CREATE TABLE `livros` (
   `autor_id` integer
 );
 
+-- ----------------Criação da entidade de emprestimos de livors
+
 CREATE TABLE `emprestimos_livros` (
   `id_emprestimo` integer PRIMARY KEY,
   `usuario_id` integer,
   `livro_id` integer
 );
+
+-- ----------------Alteração de tabelas para add chaves estrangeira
 
 ALTER TABLE `pessoas` ADD FOREIGN KEY (`endereco_id`) REFERENCES `enderecos` (`id_endereco`);
 
@@ -64,22 +82,29 @@ ALTER TABLE `funcionarios` ADD FOREIGN KEY (`pessoa_id`) REFERENCES `pessoas` (`
 
 ALTER TABLE `usuarios` ADD FOREIGN KEY (`pessoa_id`) REFERENCES `pessoas` (`id_pessoa`);
 
+-- --------------Tabela criada automaticamente pelo Mysql ao converter o código DBDiagram
+
 CREATE TABLE `pessoas_autores` (
   `pessoas_id_pessoa` integer,
   `autores_pessoa_id` integer,
   PRIMARY KEY (`pessoas_id_pessoa`, `autores_pessoa_id`)
 );
 
+----------------Alteração de tabelas para add chaves estrangeira              
+
 ALTER TABLE `pessoas_autores` ADD FOREIGN KEY (`pessoas_id_pessoa`) REFERENCES `pessoas` (`id_pessoa`);
 
 ALTER TABLE `pessoas_autores` ADD FOREIGN KEY (`autores_pessoa_id`) REFERENCES `autores` (`pessoa_id`);
 
+-- ---------------Tabela criada automaticamente pelo Mysql ao converter o código DBDiagram
 
 CREATE TABLE `autores_livros` (
   `autores_id_autor` integer,
   `livros_autor_id` integer,
   PRIMARY KEY (`autores_id_autor`, `livros_autor_id`)
 );
+
+-- ----------------Alteração de tabelas para add chaves estrangeira 
 
 ALTER TABLE `autores_livros` ADD FOREIGN KEY (`autores_id_autor`) REFERENCES `autores` (`id_autor`);
 
